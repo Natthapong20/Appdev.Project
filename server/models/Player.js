a609ef9f4dd36a622672c7baecd7ec0f5bf91939
phuettiwut <prethiwut@gmail.com>
2025-03-12 08:18:42 +0700

    สร้างให้สร้าง schema ในดาต้าเบส

---
+++ server/models/Player.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = new mongoose.Schema({
+    PlayerName: String,
+    Age: Number,
+    Position: String,
+    Height: String,
+    Nationality: String,
+    Price: String,
+    Goals: Number,
+    Assists: Number,
+    Appearances: Number,
+    Cleansheet: Number,
+    image_url: String
+});
+
+const playerl = mongoose.model("player", EmployeeSchema);
+
+module.exports = playerl;
+
+//อันนี้สร้าง ให้เก็บข้อมูลใน database ได้
