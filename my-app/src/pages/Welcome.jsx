5a64f05b57f84655116ffe1b93207c11f01b1f49
suptin0 <jednipit.i@gmail.com>
2025-03-29 08:04:11 +0700

    SIU

--- my-app/src/pages/Welcome.jsx
+++ my-app/src/pages/Welcome.jsx
@@ -1,61 +1,55 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import picture from "./picture/yamal.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import picture from "./picture/yamal.png";
+import siu from "./picture/RonaloSiu.png";
+import Ooo from "./picture/messiO.png";
 
+function Welcome() {
+  return (
+    <div className="relative min-h-screen bg-[#060143] text-white">
+     
+      <h1 className="text-center text-5xl font-bold">Welcome To Master League</h1>
 
-function  Welcome(){
-    
-
-    return(
-    <div>
-    
-    <div style = {{ //กล่องบน
-            width: "100%",
-            height: "20%",
-            backgroundColor: "#060143",
-            position: "absolute",
-            left:"0px",
-            top:"0",
-            
-            
-        }}></div>
-    
-        <h1 style={{
-        
-        color:"white",
-        position:"relative",
-        top:"20%",
-        textAlign:"center"
-        }}>Welcome</h1>
-       
-       
-       <div style={{ textAlign: "center", marginTop: "200px",}}>
-        <img
-          src={picture}
-          alt="Yamal"
+        {/* Image */}
+        <div className="flex justify-center mt-12">
+         <img
+           src={picture}
+           alt="Yamal"
+           className="w-1/4 h-auto rounded-full shadow-lg border-4 border-white"
+           />
+           <img
+          src={siu}
+          alt="SIU"
           style={{
-            float:"right",
-            width: "25%",
+            position: "absolute",
+            left: "5%",
+            bottom: "20%",
+            width: "20%",
             height: "auto",
           }}
         />
-      </div>
-      <Link to="/register" className="btn btn-danger w-auto rounded-auto">START</Link>
-    
-        <div style = {{ //กล่องล่าง
-            width: "100%",
-            height: "20%",
-            backgroundColor: "#062342",
+        <img
+          src={Ooo}
+          alt="OOO"
+          style={{
             position: "absolute",
-            left:"0px",
-            bottom:"0",
-            
-        }}></div>
-    
-   
-    
-    </div>
-
-);
-}export default Welcome;
+            right: "0%",
+            bottom: "20%",
+            width: "35%",
+            height: "auto",
+          }}
+        />
+         </div>
+         <div className="text-center mt-16">
+         <Link
+           to="/register"
+           className="bg-gradient-to-r from-[#f12711] to-[#f5af19] text-white py-3 px-8 rounded-lg text-xl font-semibold shadow-lg hover:scale-105 transform transition duration-300"
+         >
+           START
+         </Link>
+       </div>
+       <h1 className="text-center mt-5 text-5xl font-bold">ปลุกอารมณ์ฟุตบอลในตัวคุณ</h1>
+     </div>
+   );
+ }
+ export default Welcome;
