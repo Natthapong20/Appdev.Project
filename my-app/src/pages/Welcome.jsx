6121661b3836da3de717f3c120902b55f51f94d7
phuettiwut <prethiwut@gmail.com>
2025-03-29 11:52:00 +0700

    Update Welcome.jsx

--- my-app/src/pages/Welcome.jsx
+++ my-app/src/pages/Welcome.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import picture from "./picture/yamal.png";
-import siu from "./picture/RonaloSiu.png";
-import Ooo from "./picture/messiO.png";
 
 function Welcome() {
   return (
@@ -10,46 +8,27 @@ function Welcome() {
      
       <h1 className="text-center text-5xl font-bold">Welcome To Master League</h1>
 
-        {/* Image */}
-        <div className="flex justify-center mt-12">
-         <img
-           src={picture}
-           alt="Yamal"
-           className="w-1/4 h-auto rounded-full shadow-lg border-4 border-white"
-           />
-           <img
-          src={siu}
-          alt="SIU"
-          style={{
-            position: "absolute",
-            left: "5%",
-            bottom: "20%",
-            width: "20%",
-            height: "auto",
-          }}
-        />
+      {/* Image */}
+      <div className="flex justify-center mt-12">
         <img
-          src={Ooo}
-          alt="OOO"
-          style={{
-            position: "absolute",
-            right: "0%",
-            bottom: "20%",
-            width: "35%",
-            height: "auto",
-          }}
+          src={picture}
+          alt="Yamal"
+          className="w-1/4 h-auto rounded-full shadow-lg border-4 border-white"
         />
-         </div>
-         <div className="text-center mt-16">
-         <Link
-           to="/register"
-           className="bg-gradient-to-r from-[#f12711] to-[#f5af19] text-white py-3 px-8 rounded-lg text-xl font-semibold shadow-lg hover:scale-105 transform transition duration-300"
-         >
-           START
-         </Link>
-       </div>
-       <h1 className="text-center mt-5 text-5xl font-bold">ปลุกอารมณ์ฟุตบอลในตัวคุณ</h1>
-     </div>
-   );
- }
- export default Welcome;
+      </div>
+
+    
+      <div className="text-center mt-16">
+        <Link
+          to="/register"
+          className="bg-gradient-to-r from-[#f12711] to-[#f5af19] text-white py-3 px-8 rounded-lg text-xl font-semibold shadow-lg hover:scale-105 transform transition duration-300"
+        >
+          START
+        </Link>
+      </div>
+      <h1 className="text-center mt-5 text-5xl font-bold">ปลุกอารมณ์ฟุตบอลในตัวคุณ</h1>
+    </div>
+  );
+}
+
+export default Welcome;
